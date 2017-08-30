c353538fe1d0f3ec4778bb947eee1a1ad666f917
Daniela <d.marino10@uniandes.edu.co>
2017-08-30 02:12:59 -0500

    skills

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#skills-button").click(function(){
+        jQuery('.skillbar').each(function(){
+		    jQuery(this).find('.skillbar-bar').animate({
+			    width:jQuery(this).attr('data-percent')
+		    },6000);
+	    });
+    });
+});
